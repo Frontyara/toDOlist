052002f49870a377e0021f439d2d458a634f55bd
Autor <drop1for1me@gmail.com>
2025-02-25 10:43:06 +0500

    add disabled behavior in default button ui-kit

--- src/shared/buttons/defaultButton/index.tsx
+++ src/shared/buttons/defaultButton/index.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import styled from "styled-components";
+import colors from "../../sharedOfShared/colors";
 
 interface stylesButton {
   background?: string;
   border?: string;
-  isDisabled: boolean;
   color?: string;
+  isDisabled: boolean;
   text: string;
   width?: number;
   height?: number;
@@ -22,24 +23,41 @@ export const DefaultButton: FC<stylesButton> = ({
   height,
   onClick,
 }) => {
-    const defBackground = background || "#304ffe"
-    const defBorder = border || 'none'
-    const defColor = color || "#fff"
-    const defWidth = width || 120
-    const defHeight = height || 40
-  const Button = styled.button`
-    background: ${defBackground};
-    border: 1px solid ${defBorder};
-    border-radius: 10px;
-    color: ${defColor};
-    font-weight: 600;
-    width: ${defWidth}px;
-    height: ${defHeight}px;
-    transition: all 0.2s ease;
-    &:hover {
-      background: ${"#0026CA"};
-    }
-  `;
+  const defBackground = background || colors.deepBlue;
+  const defBorder = border || "none";
+  const defColor = color || colors.white;
+  const defWidth = width || 120;
+  const defHeight = height || 40;
+
+  if (!isDisabled) {
+    var Button = styled.button`
+      background: ${defBackground};
+      border: 1px solid ${defBorder};
+      border-radius: 10px;
+      color: ${defColor};
+      font-weight: 600;
+      width: ${defWidth}px;
+      height: ${defHeight}px;
+      transition: all 0.2s ease;
+      &:hover {
+        background: ${"#0026CA"};
+      }
+    `;
+  } else {
+    var Button = styled.button`
+      background: none;
+      border: 1px solid ${colors.disabledColor};
+      border-radius: 10px;
+      color: ${colors.disabledColor};
+      font-weight: 600;
+      width: ${defWidth}px;
+      height: ${defHeight}px;
+      transition: all 0.2s ease;
+      &:hover{
+      box-shadow: 0px 1px 2px ${colors.disabledColor}, inset 0px 0px 4px ${colors.disabledColor};
+      }
+    `;
+  }
   return (
     <Button onClick={() => onClick()} disabled={isDisabled}>
       {text}
