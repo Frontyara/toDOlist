44a546cd322d9328e4900b361e1259256294df1c
Autor <drop1for1me@gmail.com>
2025-02-25 10:41:51 +0500

    add def colors ui-kit

---
+++ src/shared/sharedOfShared/colors/index.ts
@@ -0,0 +1,20 @@
+export interface tColors {
+  errorColor: string;
+  disabledColor: string;
+  deepBlue: string;
+  seaBlue: string;
+  white: '#fff';
+  black: '#000';
+  [key: string]: string;
+}
+
+const colors: tColors = {
+  errorColor: "#EB5252",
+  disabledColor: "#a6a6a6",
+  deepBlue: "#304ffe",
+  seaBlue: "#73a2ff",
+  white: '#fff',
+  black: '#000',
+};
+
+export default colors;
